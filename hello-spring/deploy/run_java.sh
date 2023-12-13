9e1a40aac11107558e47a4ea23686b090e4dccf3
boham97 <kangbosung97@gmail.com>
2023-12-13 16:06:01 +0900

    이미지 빌드 에러수정;

--- hello-spring/deploy/run_java.sh
+++ hello-spring/deploy/run_java.sh
@@ -2,5 +2,5 @@
 cd /opt/cdtest
 echo "배포"
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-sudo docker build -t hellospring:$version
+sudo docker build -t hellospring:$version .
 sudo docker run -d -p 8080:8080 --name hellospring hellospring:$version
