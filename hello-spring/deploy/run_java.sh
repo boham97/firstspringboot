58ff281221535871523169e3bd4980257b7f5595
boham97 <kangbosung97@gmail.com>
2023-12-13 16:18:10 +0900

    도커파일 arg 수정

--- hello-spring/deploy/run_java.sh
+++ hello-spring/deploy/run_java.sh
@@ -2,5 +2,8 @@
 cd /opt/cdtest
 echo "배포"
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
+echo $version
 sudo docker build -t hellospring:$version .
+echo "build"
 sudo docker run -d -p 8080:8080 --name hellospring hellospring:$version
+echo "successfully run!"
